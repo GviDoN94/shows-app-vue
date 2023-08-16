5514f9669ab57c642d17b5afa2222d5f76720439
Vladislav <vlad_surin@mail.ru>
2023-08-16 14:08:59 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import BreakingBadCards from '@/components/BreakingBadCards';
+</script>
+
+<template>
+  <main>
+    <h1>HERO</h1>
+    <BreakingBadCards />
+  </main>
+</template>
